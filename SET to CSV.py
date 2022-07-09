bef9cbdbf8e7679b3705159d839f0efb099f07b7
Ben <benjaminwolfaardt@gmail.com>
2022-07-09 21:29:27 +0200

    Increase code readibility

--- SET to CSV.py
+++ SET to CSV.py
@@ -1,27 +1,54 @@
+import sys
+import os
 import mne
 import pandas as pd
 import numpy as np
 
-Numbers = [1,2,3,4,5,6]
+DIRECTORY_SET_DATA_ROOT = "E:\\Skripsie\\Data\\1-Pre_Processed-edited"
+DIRECTORY_CSV_DATA_OUTPUT = "E:\\Skripsie\\Data\\2-CSV-edited"
+
+Names = ['A', 'B', 'C', 'D']
 Triggers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
 
-for number in Numbers:
+for name in Names:
     for trigger in Triggers:
         try:
-            if trigger <= Triggers[8]:
-                df = mne.io.read_epochs_eeglab('G:\\a Data\\0 Preprocessed\\Leroy\\epoch\\L Test{} T0{}.set'.format((number),(trigger)))
+            # Set LEADING_ZERO based on naming convention 
+            # a 0 prefix has been used for triggers < 9
+            # eg: 01
+            if trigger <= 9:
+                LEADING_ZERO = "0"
             else:
-                df = mne.io.read_epochs_eeglab('G:\\a Data\\0 Preprocessed\\Leroy\\epoch\\L Test{} T{}.set'.format((number),(trigger)))
-                
-            i = 0
-            for epoch in df:
-                i=i+1
-                dfNew1 = pd.DataFrame(epoch)
-                if trigger <= Triggers[8]:
-                    np.savetxt("L Test"+str(number)+" T0"+str(trigger)+" E"+format(i,'02d')+".csv",dfNew1,delimiter=',')
-                else:
-                    np.savetxt("L Test"+str(number)+" T"+str(trigger)+" E"+format(i,'02d')+".csv",dfNew1,delimiter=',')
+                LEADING_ZERO = ""
 
-        except IOError:
-            print("Error")
-print("DONE")  
+            # Read in the SET file
+            directory = f"{DIRECTORY_SET_DATA_ROOT}\{name}"
+            filename = f"T{LEADING_ZERO}{trigger}.set"
+            df = mne.io.read_epochs_eeglab(f"{directory}\{filename}")
+ 
+            i = 0
+
+            for epoch in df:
+                # Create output directory if it does not exist
+                if i == 0:
+                    try: 
+                        os.makedirs(f"{DIRECTORY_CSV_DATA_OUTPUT}\{name}", exist_ok=True)
+                    except:
+                        print("The path already exists")
+                
+                i += 1
+
+                # Save CSV in desired location
+                dfEpoched = pd.DataFrame(epoch)
+                directory = f"{DIRECTORY_CSV_DATA_OUTPUT}\{name}"
+                filename = f"T{LEADING_ZERO}{trigger}_{i:03d}.csv"
+                np.savetxt(f"{directory}\{filename}", dfEpoched, delimiter=',')
+
+        except IOError as e:
+            # Print error message and remind user that an error needs to be adressed afterwards
+            print(f"Error: {e}")
+            print("")
+            print(f"An error occurded for participant {name} in trigger {trigger}.")
+            print("Remeber to check back and correct this!")
+
+print("Sucessfully converted SET files to CSVs")
