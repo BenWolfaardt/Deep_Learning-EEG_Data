876bae288c24f27b027a487a59fe5375455ea325
Ben Wolfaardt <ben.wolfaardt@bitpanda.com>
2022-09-08 21:24:40 +0200

    Add TODO

--- setup/scripts/configure_env.m1.sh
+++ setup/scripts/configure_env.m1.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 conda create --prefix ./env python=3.9 -y
-# sleep 1
 echo ""
 echo "Env created, now time to install all of the libraries and dependancies"
 echo ""
@@ -10,6 +9,6 @@ conda install -c apple tensorflow-deps -y
 python -m pip install pip-tools
 pip-compile --output-file=- > requirements.txt
 GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1 GRPC_PYTHON_BUILD_SYSTEM_ZLIB=1 pip install grpcio==1.47.0
-# Add step to remove grpcio from the requirements.txt so that the next step doesn't error on the  reinstall attempt
+# TODO Add step to remove grpcio from the requirements.txt so that the next step doesn't error on the  reinstall attempt
 pip install -r requirements.txt
 pip list
