d1acc2e8f1e668760bc512842e00a71e12b85d2d
Ben Wolfaardt <ben.wolfaardt@bitpanda.com>
2022-09-15 21:36:40 +0200

    Refactor and make use of yaml config

--- 3-Create_Model-Original.py
+++ 3-Create_Model-Original.py
@@ -6,165 +6,195 @@ Authors: William & Ben
 import numpy as np
 import pickle
 
+from envyaml import EnvYAML
 from keras.callbacks import EarlyStopping,  ModelCheckpoint, TensorBoard, ReduceLROnPlateau
 from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 from keras.models import Sequential
 from nptyping import NDArray
 from sklearn.model_selection import StratifiedKFold
 
-EXPERIMENT = "Siobhan"
-TRIGGERS = ['Left','Right']
-COMPARISON = {0: "All", 1: "Single"}
-PARTICIPANTS = ["5"]
-# PARTICIPANTS = [2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
-NAME = f"{EXPERIMENT}_{COMPARISON[1]}-{PARTICIPANTS[0]}"
-DIRECTORY_PICKLE_DATA_INPUT = "/Users/james.wolfaardt/code/__ben/Code/Deep_Learning-EEG_Data/outputs/pickles"
-DIRECTORY_MODEL_DATA_OUTPUT = "/Users/james.wolfaardt/code/__ben/Code/Deep_Learning-EEG_Data/outputs/models"
-# TODO note duplicate
-participant = ["5"]
-KFOLD_SPLITS=5
-EPOCHS=20
-
 class Create:
     def __init__(self) -> None:
-        pass
+        # config
+        self.os: str = None
+        self.config: EnvYAML = None
+        self.experiment: str = None
+        # paths
+        self.pickles: str = None
+        self.models: str = None
+        # experiment details
+        self.name: str = None
+        self.participants: list[int] = []
+        self.triggers: list[str] = []
+        self.comparison: int = None
+        # model
+        self.kfold: int = None
+        self.epochs: int = None
+
+        def callbacks(self) -> None:
+            self.callbacks: list = []
+            
+            # This is the callback for writing checkpoints during training
+            path_checkpoint = 'checkpoint.keras'
+            callback_checkpoint = ModelCheckpoint(
+                filepath=path_checkpoint,
+                monitor='val_loss',
+                verbose=1,
+                save_weights_only=True,
+                save_best_only=True,
+            )
+            # This is the callback for stopping the optimization when performance worsens on the validation-set
+            callback_early_stopping = EarlyStopping(
+                monitor='val_loss',
+                patience=3,
+                verbose=1,
+                # restore_best_weights=True,
+            )
+            # This is the callback for writing the TensorBoard log during training.
+            callback_tensorboard = TensorBoard(
+                log_dir='./logs/original/',
+                histogram_freq=0,
+                write_graph=False,
+            )
+            # This callback reduces the learning-rate for the optimizer if the validation-loss has not improved 
+            # since the last epoch (as indicated by patience=0). The learning-rate will be reduced by multiplying 
+            # it with the given factor. We set a start learning-rate of 1e-3 above, so multiplying it by 0.1 gives 
+            # a learning-rate of 1e-4. We don't want the learning-rate to go any lower than this.
+            callback_reduce_lr = ReduceLROnPlateau(
+                monitor='val_loss',
+                factor=0.1,
+                min_lr=1e-4,
+                patience=0,
+                verbose=1,
+            )
+
+            self.callbacks = [
+                # callback_early_stopping,
+                # callback_checkpoint,
+                # callback_tensorboard,
+                # callback_reduce_lr
+            ]
+
+        callbacks(self)
+
+    def load_yaml(self) -> None:
+        self.config = EnvYAML("./setup/config.yaml", strict=False)
+
+    def populate_config(self) -> None:
+        # TODO parse in as cli command from make file
+        self.os = "mac_m1"
+        self.experiment = "libet"
+
+        self.pickles = self.config[f"os.{self.os}.io_paths.pickle_files"]
+        self.models = self.config[f"os.{self.os}.io_paths.model_files"]
+        # TODO use name for folder or something
+        self.name = self.config[f"experiment.details.{self.experiment}.name"]
+        self.participants = self.config[f"experiment.details.{self.experiment}.participants"]
+        self.triggers = self.config[f"experiment.details.{self.experiment}.triggers"]
+        self.kfolds = self.config[f"model_parameters.kfolds"]
+        self.epochs = self.config[f"model_parameters.epochs"]
+        self.comparison = self.config[f"model_parameters.comparison"]
 
     # Load preprocessed training/test pickles
-    def load_data(self)  -> tuple[NDArray, NDArray]:
-        with open(f"{DIRECTORY_PICKLE_DATA_INPUT}/X-{str(participant[0])}-Training.pickle", 'rb') as f:
-            X = pickle.load(f) # shape: (1369, 63, 450, 1)
-            X = np.asarray(X)
-        with open(f"{DIRECTORY_PICKLE_DATA_INPUT}/y-{str(participant[0])}-Training.pickle", 'rb') as f:
-            y = pickle.load(f) # shape: (
-            y = np.transpose(y)
-        return X, y
+    def load_data(self) -> tuple[NDArray, NDArray]:
+        # TODO assign type hints
+        self.X = None
+        self.y = None
+        
+        with open(f"{self.pickles}/X-{str(self.participant)}-Training.pickle", 'rb') as f:
+            self.X = pickle.load(f) # shape: (1369, 63, 450, 1)
+            self.X = np.asarray(self.X)
+        with open(f"{self.pickles}/y-{str(self.participant)}-Training.pickle", 'rb') as f:
+            self.y = pickle.load(f) # shape: (
+            self.y = np.transpose(self.y)
 
     # Create model 
-    def create_model(self, X) -> Sequential():
-        model = Sequential()
-        model.add(Conv2D(256, (3, 3), input_shape=X.shape[1:])) # input_shape: (63, 450, 1)
-        model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
+    def create_model(self) -> None:
+        self.model: Sequential = Sequential()
+              
+        self.model.add(Conv2D(256, (3, 3), input_shape=self.X.shape[1:])) # input_shape: (63, 450, 1)
+        self.model.add(Activation('relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
         
-        model.add(Conv2D(128, (3, 3)))
-        model.add(Activation('relu')) 
+        self.model.add(Conv2D(128, (3, 3)))
+        self.model.add(Activation('relu')) 
         
-        model.add(Conv2D(64, (3, 3)))
-        model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.2))
+        self.model.add(Conv2D(64, (3, 3)))
+        self.model.add(Activation('relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.2))
         
-        model.add(Conv2D(32, (3, 3)))
-        model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.2))
+        self.model.add(Conv2D(32, (3, 3)))
+        self.model.add(Activation('relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.2))
             
-        model.add(Flatten())   
+        self.model.add(Flatten())   
         
-        model.add(Dense(32))
-        model.add(Activation('relu'))
+        self.model.add(Dense(32))
+        self.model.add(Activation('relu'))
         
-        model.add(Dense(16))
-        model.add(Activation('relu'))
-        model.add(Dropout(0.2)) 
+        self.model.add(Dense(16))
+        self.model.add(Activation('relu'))
+        self.model.add(Dropout(0.2)) 
         
         # Last dense 1ayers must have number of classes in data in the parenthesis
-        model.add(Dense(len(TRIGGERS)))
+        self.model.add(Dense(len(self.triggers)))
         # For a binary classification you should have a sigmoid last layer
         # See 2nd answer for more details: https://stackoverflow.com/questions/68776790/model-predict-classes-is-deprecated-what-to-use-instead
         # softmax for other experiment
         # TODO add automatic logic for this
-        model.add(Activation('sigmoid'))
+        self.model.add(Activation('sigmoid'))
 
-        model.compile(loss='sparse_categorical_crossentropy',
+        self.model.compile(loss='sparse_categorical_crossentropy',
                         optimizer='adam',
                         metrics=['accuracy'])
 
         # print(model.summary())
 
-        return model
-
     # Train model with K-fold cross validation with early stopping
-    def kfold_cross_validation(
-        self, 
-        model: Sequential(),
-        X,
-        y,
-        kfold: StratifiedKFold,
-    ) -> Sequential():
-        # This is the callback for writing checkpoints during training
-        path_checkpoint = 'checkpoint.keras'
-        callback_checkpoint = ModelCheckpoint(
-            filepath=path_checkpoint,
-            monitor='val_loss',
-            verbose=1,
-            save_weights_only=True,
-            save_best_only=True,
-        )
-        # This is the callback for stopping the optimization when performance worsens on the validation-set
-        callback_early_stopping = EarlyStopping(
-            monitor='val_loss',
-            patience=3, 
-            verbose=1, 
-            # restore_best_weights=True,
-        )
-        # This is the callback for writing the TensorBoard log during training.
-        callback_tensorboard = TensorBoard(
-            log_dir='./logs/original/',
-            histogram_freq=0,
-            write_graph=False,
-        )
-        # This callback reduces the learning-rate for the optimizer if the validation-loss has not improved 
-        # since the last epoch (as indicated by patience=0). The learning-rate will be reduced by multiplying 
-        # it with the given factor. We set a start learning-rate of 1e-3 above, so multiplying it by 0.1 gives 
-        # a learning-rate of 1e-4. We don't want the learning-rate to go any lower than this.
-        callback_reduce_lr = ReduceLROnPlateau(
-            monitor='val_loss',
-            factor=0.1,
-            min_lr=1e-4,
-            patience=0,
-            verbose=1,
-        )
+    def kfold_cross_validation(self) -> None:
+        kfold: StratifiedKFold = None
+        cvscores: list = []
 
-        callbacks = [
-            callback_early_stopping,
-            callback_checkpoint,
-            callback_tensorboard,
-            # callback_reduce_lr
-        ]
+        kfold = StratifiedKFold(n_splits=self.kfolds, shuffle=True, random_state=42)
 
-        # tqdm_callback = TqdmCallback()
-        cvscores = []
-        for index, (train_index, validation_index) in enumerate(kfold.split(X, y)):
-            print(f"\nTraining on fold: {str(index+1)}/{KFOLD_SPLITS}\n")
+        print(f"\nTraining model for particiapnt: {self.participant}")
+
+        for index, (train_index, validation_index) in enumerate(kfold.split(self.X, self.y)):
+            print(f"Training on fold: {str(index+1)}/{self.kfolds}\n")
             
             #Generate batches
-            Xtrain, Xval = X[train_index], X[validation_index]
-            ytrain, yval = y[train_index], y[validation_index]
+            Xtrain, Xval = self.X[train_index], self.X[validation_index]
+            ytrain, yval = self.y[train_index], self.y[validation_index]
 
-            model.fit(Xtrain,ytrain, epochs=EPOCHS, batch_size=10, verbose=1, validation_data=(Xval,yval), callbacks=callbacks, shuffle=True)
+            self.model.fit(Xtrain,ytrain, epochs=self.epochs, batch_size=10, verbose=1, validation_data=(Xval,yval), callbacks=self.callbacks, shuffle=True)
             # history = model.fit(Xtrain,ytrain, epochs=EPOCHS, batch_size=10, verbose=1, validation_data=(Xval,yval), callbacks=[early_stopping], shuffle=True)
             # accuracy_history = history.history['accuracy']
             # val_accuracy_history = history.history['val_accuracy']
 
-            scores = model.evaluate(Xval, yval, verbose=1)
+            scores = self.model.evaluate(Xval, yval, verbose=1)
             cvscores.append(scores[1] * 100)
 
-        print(f"\nEvaluated acuracy: {np.mean(cvscores):.2f}% (+/- {np.std(cvscores):.2f}%) on {KFOLD_SPLITS} k-folds")
+            print()
 
-        return model
+        print(f"\nEvaluated acuracy: {np.mean(cvscores):.2f}% (+/- {np.std(cvscores):.2f}%) on {self.kfolds} k-folds")
 
     # Save model to disk
-    def save_model(self, model: Sequential()) -> None:
-        model.save(f"{DIRECTORY_MODEL_DATA_OUTPUT}/{NAME}.h5")
+    def save_model(self) -> None:
+        # TODO don't save coparison as number but rather as value of dict {0: "All", 1: "Single"}
+        self.filename = f"{self.experiment}_{self.comparison}-{self.participant}"
+        self.model.save(f"{self.models}/{self.filename}.h5")
     
     def setup_and_test_data(self) -> None:
-        X, y, = self.load_data()
-        model = self.create_model(X)
-        kfold = StratifiedKFold(n_splits=KFOLD_SPLITS, shuffle=True, random_state=42)
-        model = self.kfold_cross_validation(model, X, y, kfold)
-        self.save_model(model)
+        for self.participant in self.participants:
+            self.load_data()
+            self.create_model()
+            self.kfold_cross_validation()
+            self.save_model()
 
 if __name__ == '__main__':
     app = Create()
+    app.load_yaml()
+    app.populate_config()
     app.setup_and_test_data()
